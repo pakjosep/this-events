949145f01f68dd5502bf32e270b7d9ca043bb08c
Joseph Pak <pakjosep@Josephs-MacBook-Air.local>
2017-08-29 13:55:23 -1000

    initial

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 //Secret Menu Challenge!
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
+var restaurant=document.getElementsByClassName("name");
+for(var i = 0; i<restaurant.length; i++){
+  restaurant[i].addEventListener("click", showMenu);
 
+}
 
+function showMenu(){
+  var showMenu = this.querySelectorAll(".menu")[0];
+  if(showMenu.style.display === "none"){
+    showMenu.style.display = "block";
+  }else{
+    showMenu.style.display = "none"
+  }
+}
